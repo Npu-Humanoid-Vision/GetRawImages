167f940cba770fcf3774a20bf7891b222f1b1997
Alex_Beng <599267065@qq.com>
2019-02-10 00:08:23 +0800

    merge phone proj with cam proj together

---
+++ GetImgFromPhone.py
@@ -0,0 +1,17 @@
+import os
+import datetime
+import cv2
+
+def GetImgs(root_path_in_phone, save_path_in_pc):
+
+    date = datetime.datetime.now().strftime("%Y%m%d")
+
+    result = os.popen("adb shell ls "+ root_path_in_phone).read()
+    result_list = [i for i in result.split() if i != '' and date in i]
+    for i in result_list:
+        os.system("adb pull " + root_path_in_phone+i + save_path_in_pc)
+
+    return 
+
+if __name__ == "__main__":
+    GetImgs("/storage/0E6F-D222/DCIM/Camera/", " ../../BackUpSource/Ball/Train/Raw/")   
